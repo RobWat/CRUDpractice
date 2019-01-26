7b9a8b8d1e041a68be627833a3c935e77c862890
RobWat <find@A5-004015.localdomain>
2019-01-25 17:22:28 -0700

    mmade changes to fix it, also added comments starting to break down what the javascript code does. This version DOES work perfectly

--- app.js
+++ app.js
@@ -2,33 +2,38 @@ function newElement() {
   var li = document.createElement("li")
   var inputValue = document.getElementById("myInput").value
   var t = document.createTextNode(inputValue)
-  
+// appendChild() is to add something to the end of something, like a child
   li.appendChild(t)
 
-  if (inputValue === ""){
-    alert('Please fill out form before submitting you bich')
-  } else {
+// will alert you if text box is emtpy when clicking add  
+  if (inputValue === "") {
+    alert('Please fill out form before submitting you')
+  } else { /* else statment will  */
     document.getElementById("myUL").appendChild(li)
   }
-  document.getElementById("myInput").value = ""
 
-  var span = document.createElement("SPAN")
-  var txt = document.createTextNode("\u00D7")
+  document.getElementById("myInput").value = ""// resets text box to blank
+
+// SPAN is like div, has no meaning and is used for id and class, 
+// but the difference is SPAN is an inline element, meaning it 
+// can be on a line with other elements
+  var span = document.createElement("SPAN") 
+  var txt = document.createTextNode("\u00D7") // the x at the end
   span.className = 'close'
   span.appendChild(txt)
   li.appendChild(span)
 
+
   for(i = 0; i < close.length; i++){
     close[i].onclick = function (){
-      var div = this.parentElement
-      div.style.display = "none"
+      var div = this.parentElement;
+      div.parentElement.removeChild(div)
     }
   }
 }
 
-// delete
-
-var todos = document.getElementsByTagName("LI")
+// delete (two ways of making it happen)
+var todos = document.getElementsByTagName("LI") // the list?
 var i;
 for (i = 0; i < todos.length; i++){
   var span = document.createElement("SPAN")
@@ -36,13 +41,14 @@ for (i = 0; i < todos.length; i++){
   todos[i].appendChild(span)
 }
 
-var close = document.getElementsByClassName('close')
-var i;
-for ( i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = 'none';
-  }
+
+var close = document.getElementsByClassName("close")
+var i
+for ( i = 0; i < close.length; i++ ) {
+    close[i].onlick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
 }
 
 
